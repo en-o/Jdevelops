6a933f418ee4f8dd7c798e46a4ef8772a2782a45
en-o <13320385140@163.com>
2021-01-30 21:23:11 +0800

    更改版本号

---
+++ component-aops/aops-apiVersion/src/main/java/com/detabes/version/handler/CustomRequestMappingHandlerMapping.java
@@ -0,0 +1,34 @@
+package com.detabes.version.handler;
+
+import com.detabes.version.annotation.ApiVersion;
+import com.detabes.version.config.ApiVersionCondition;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.web.servlet.mvc.condition.RequestCondition;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.lang.reflect.Method;
+
+/**
+ * 版本控制注解
+ * @author tnnn
+ * @link https://www.cnblogs.com/amuge/articles/13821162.html
+ */
+public class CustomRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
+
+
+    @Override
+    protected RequestCondition<?> getCustomTypeCondition(Class<?> handlerType) {
+        ApiVersion apiVersion = AnnotationUtils.findAnnotation(handlerType,ApiVersion.class);
+        return createCondition(apiVersion);
+    }
+
+    @Override
+    protected RequestCondition<?> getCustomMethodCondition(Method method) {
+        ApiVersion apiVersion = AnnotationUtils.findAnnotation(method,ApiVersion.class);
+        return  createCondition(apiVersion);
+    }
+
+    private RequestCondition<ApiVersionCondition> createCondition(ApiVersion apiVersion) {
+        return apiVersion == null ? null : new ApiVersionCondition(apiVersion.value());
+    }
+}
