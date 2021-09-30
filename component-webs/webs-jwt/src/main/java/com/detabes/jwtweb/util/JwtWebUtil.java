b5663bcbca8618432a57fba31870ee9a9ea0cc15
en-o <13320385140@163.com>
2021-09-30 09:41:53 +0800

    【jwt】 新增解析用户相关工具
    (cherry picked from commit 6ecfe8059f725de1fce02cdc42755212c3022270)

--- component-webs/webs-jwt/src/main/java/com/detabes/jwtweb/util/JwtWebUtil.java
+++ component-webs/webs-jwt/src/main/java/com/detabes/jwtweb/util/JwtWebUtil.java
@@ -1,8 +1,11 @@
 package com.detabes.jwtweb.util;
 
+import com.detabes.json.GsonUtils;
+import com.detabes.jwt.util.JwtUtil;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * @author tn
@@ -25,5 +28,32 @@ public class JwtWebUtil {
         }
         token = request.getParameter(tokenName);
         return token;
+    }
+
+
+    /**
+     * 获取token中的  remark 数据并 转化成 T类型
+     * @param request request
+     * @param t 返回类型 （颁发时token存储的类型）
+     * @return t
+     */
+    public static <T> T getTokenUserInfoByRemark(HttpServletRequest request, Class<T> t ){
+        String token = JwtWebUtil.getToken(request);
+        Map<String, Object> loginNames = JwtUtil.parseJwt(token);
+        String remark = loginNames.get("remark").toString();
+        return GsonUtils.getGson().fromJson(remark, t);
+    }
+
+
+    /**
+     * 获取token中的  remark 数据并 转化成 T类型
+     * @param token token
+     * @param t 返回类型 （颁发时token存储的类型）
+     * @return t
+     */
+    public static  <T> T  getTokenUserInfoByRemark(String token, Class<T> t ){
+        Map<String, Object> loginNames = JwtUtil.parseJwt(token);
+        String remark = loginNames.get("remark").toString();
+        return GsonUtils.getGson().fromJson(remark, t);
     }
 }
