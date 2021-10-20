d30bb1dce5e32636e484ffa9dc1908895f8d42a2
en-o <13320385140@163.com>
2021-10-20 14:53:38 +0800

    [ISSUE #8] beanCopy无法处理时间

--- component-utils/utils-list/src/main/java/com/detabes/list/core/ListTo.java
+++ component-utils/utils-list/src/main/java/com/detabes/list/core/ListTo.java
@@ -1,6 +1,6 @@
 package com.detabes.list.core;
 
-import com.google.common.collect.Lists;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
@@ -12,6 +12,7 @@ import java.util.stream.Collectors;
 
 /**
  * 复制相关
+ *
  * @author tn
  * @version 1
  * @ClassName ListTo
@@ -22,32 +23,43 @@ public class ListTo {
 
     /**
      * 复制bean
+     *
      * @param tClass 返回类型
-     * @param list 数据
-     * @param <T> 泛型
-     * @param <K> 泛型
+     * @param list   数据
+     * @param <T>    泛型
+     * @param <K>    泛型
      * @return tClass
      */
-    public static <T, K> List<T> to(Class<T> tClass, List<K> list){
+    public static <T, K> List<T> to(Class<T> tClass, List<K> list) {
         return list.stream().map((entity) -> beanTo(tClass, entity)).collect(Collectors.toList());
     }
 
     /**
      * 复制bean
+     * 无法处理时间
      * @param tClass 需求bean
-     * @param t 被复制的bean
+     * @param t      被复制的bean
      * @return tClass
      */
-    private static <T> T beanTo(Class<T> tClass,Object t){
+    private static <T> T beanTo(Class<T> tClass, Object t) {
         try {
             T tag = tClass.newInstance();
-            beanCopy(t, tag);
+            BeanCopier.copy(t, tag);
+            /* 无法处理时间 */
+//            beanCopy(t, tag);
             return tag;
         } catch (Exception var3) {
             throw new RuntimeException(var3);
         }
     }
 
+    /**
+     * 无法处理时间个数
+     *
+     * @param source 源
+     * @param target 目标
+     * @param <T>    t
+     */
     private static <T> void beanCopy(T source, T target) {
         BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
     }
@@ -57,6 +69,7 @@ public class ListTo {
         String[] result = new String[emptyNames.size()];
         return emptyNames.toArray(result);
     }
+
     private static Set<String> getNullPropertyNameSet(Object source) {
         final BeanWrapper src = new BeanWrapperImpl(source);
         java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
