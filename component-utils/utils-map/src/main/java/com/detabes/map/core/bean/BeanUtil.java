97d29610c94390e5fe8744cd08485d3761d8f937
tan <1445763190@qq.com>
2021-11-01 15:40:49 +0800

    [Map] 优化

--- component-utils/utils-map/src/main/java/com/detabes/map/core/bean/BeanUtil.java
+++ component-utils/utils-map/src/main/java/com/detabes/map/core/bean/BeanUtil.java
@@ -3,6 +3,7 @@ package com.detabes.map.core.bean;
 import java.beans.BeanInfo;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
 
 /**
  * bean
@@ -14,7 +15,8 @@ import java.beans.PropertyDescriptor;
 public class BeanUtil {
     /**
      * BeanMerge，对象属性合并
-     * @param target  最终对象
+     *
+     * @param target 最终对象
      */
     public static <M> void merge(M target, M destination) throws Exception {
         //获取目标bean
@@ -25,10 +27,44 @@ public class BeanUtil {
             if (descriptor.getWriteMethod() != null) {
                 Object defaultValue = descriptor.getReadMethod().invoke(destination);
                 //可以使用StringUtil.isNotEmpty(defaultValue)来判断
-                if(defaultValue!=null && !"".equals(defaultValue)){
+                if (defaultValue != null && !"".equals(defaultValue)) {
                     //用非空的defaultValue值覆盖到target去
                     descriptor.getWriteMethod().invoke(target, defaultValue);
                 }
+            }
+        }
+    }
+
+
+    /**
+     * 合并对象
+     * 以destination对象为主
+     *
+     * @param origin      对象1
+     * @param destination 对象2
+     * @param <T>
+     * @return 把 origin 塞到 destination
+     */
+    public static <T> void mergeObject(T origin, T destination) {
+        if (origin == null || destination == null) {
+            return;
+        }
+        if (!origin.getClass().equals(destination.getClass())) {
+            return;
+        }
+
+        Field[] fields = destination.getClass().getDeclaredFields();
+        for (int i = 0; i < fields.length; i++) {
+            try {
+                fields[i].setAccessible(true);
+                Object valueD = fields[i].get(origin);
+                Object valueO = fields[i].get(destination);
+                if (null == valueO) {
+                    fields[i].set(destination, valueD);
+                }
+                fields[i].setAccessible(false);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
