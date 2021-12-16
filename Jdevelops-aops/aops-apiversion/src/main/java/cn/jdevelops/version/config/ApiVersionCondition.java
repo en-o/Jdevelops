0d6d62657212dd97d11f1cf49f3526708a4d11ca
tan <1445763190@qq.com>
2021-12-16 14:09:53 +0800

    [ApiVersion] 优化 apiVersion

--- Jdevelops-aops/aops-apiversion/src/main/java/cn/jdevelops/version/config/ApiVersionCondition.java
+++ Jdevelops-aops/aops-apiversion/src/main/java/cn/jdevelops/version/config/ApiVersionCondition.java
@@ -5,8 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.servlet.mvc.condition.RequestCondition;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Optional;
 
 /**
  * 版本匹配
@@ -16,7 +15,6 @@ import java.util.regex.Pattern;
  */
 @Slf4j
 public class ApiVersionCondition implements RequestCondition<ApiVersionCondition> {
-    private static final Pattern VERSION_PREFIX_PATTERN = Pattern.compile("^\\S+/([1-9][.][0-9])$");
     private double apiVersion;
 
     public ApiVersionCondition(double apiVersion) {
@@ -39,21 +37,12 @@ public class ApiVersionCondition implements RequestCondition<ApiVersionCondition
     @Override
     public ApiVersionCondition getMatchingCondition(HttpServletRequest httpServletRequest) {
         try {
-            String parameter = httpServletRequest.getParameter("version");
-            String header = httpServletRequest.getHeader("version");
-            Matcher matcher = VERSION_PREFIX_PATTERN.matcher(httpServletRequest.getRequestURI());
-            if (StringUtils.isNotBlank(parameter)) {
-                Double version = Double.valueOf(parameter);
-                if (version >= this.apiVersion) {
-                    return this;
-                }
-            } else if (StringUtils.isNotBlank(header)) {
-                Double version = Double.valueOf(header);
-                if (version >= this.apiVersion) {
-                    return this;
-                }
-            } else if (matcher.find()) {
-                Double version = Double.valueOf(matcher.group(1));
+            String reqVersion = httpServletRequest.getParameter("version");
+            reqVersion = Optional.ofNullable(reqVersion)
+                    .orElse(httpServletRequest.getHeader("version"));
+            reqVersion = Optional.ofNullable(reqVersion).orElse("1.0");
+            if (StringUtils.isNotBlank(reqVersion)) {
+                double version = Double.parseDouble(reqVersion);
                 if (version >= this.apiVersion) {
                     return this;
                 }
