8fd74ded7e577ec80a0db82c775122a7c440f3ed
tan <1445763190@qq.com>
2021-12-17 12:43:38 +0800

    [优化] YmlUtils

--- jdevelops-utils/utils-spring/src/main/java/cn/jdevelops/spring/core/yml/YmlUtils.java
+++ jdevelops-utils/utils-spring/src/main/java/cn/jdevelops/spring/core/yml/YmlUtils.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
+import java.util.Objects;
 import java.util.Properties;
 
 /**
@@ -20,16 +21,19 @@ public class YmlUtils {
 	 * @param fileName 需要读取的yml文件名，如  application.yml
 	 * @param key      需要去读的键名称，如 spring.profiles.active
 	 * @return java.lang.String
-	 * @author lxw
+	 * @author tn
 	 * @date 2021/1/22 13:31
 	 */
 	public static String getValueYml(String fileName, String key) {
-
-		return getPropertiesYml(fileName).getProperty(key);
+		Properties propertiesYml = getPropertiesYml(fileName);
+		if(Objects.isNull(propertiesYml)){
+			return null;
+		}
+		return propertiesYml.getProperty(key);
 	}
 
 	/**
-	 * 获取myl文件中的所有内容，通过getProperty(key)的方法取,如：getProperty("spring.profiles.active")
+	 * 获取myl文件中的所有内容，需要哪个自己通过getProperty(key)的方法取
 	 *
 	 * @param fileName yml文件名称 ，如  application.yml
 	 * @return java.util.Properties
@@ -42,4 +46,5 @@ public class YmlUtils {
 		yamlFactory.setResources(resource);
 		return yamlFactory.getObject();
 	}
+
 }
