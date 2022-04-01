c57380189daf7f9eadfe99bca7daa59df2dc5128
tan <1445763190@qq.com>
2022-04-01 11:18:37 +0800

    [SPI] 抄的ShenYu <a href="https://shenyu.apache.org/zh/docs/index">

---
+++ jdevelops-spi/src/test/java/cn/jdevelops/spi/fixture/TestSPI.java
@@ -0,0 +1,16 @@
+package cn.jdevelops.spi.fixture;
+
+import cn.jdevelops.spi.SPI;
+
+/**
+ * 测试SPI
+ *
+ * @author tn
+ * @version 1
+ * @date 2022-04-01 11:10
+ */
+@SPI
+public interface TestSPI {
+
+    void test();
+}
