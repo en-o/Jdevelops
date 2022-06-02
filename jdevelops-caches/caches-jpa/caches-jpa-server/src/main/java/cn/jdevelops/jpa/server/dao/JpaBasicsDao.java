af9087d8b269203669b310fb231d15ba1b68dcd6
tan <1445763190@qq.com>
2022-06-02 16:27:31 +0800

    [jpa] 新增 updateEntity 指定key更新

--- jdevelops-caches/caches-jpa/caches-jpa-server/src/main/java/cn/jdevelops/jpa/server/dao/JpaBasicsDao.java
+++ jdevelops-caches/caches-jpa/caches-jpa-server/src/main/java/cn/jdevelops/jpa/server/dao/JpaBasicsDao.java
@@ -1,6 +1,8 @@
 package cn.jdevelops.jpa.server.dao;
 
 import cn.jdevelops.jap.core.util.CommUtils;
+import cn.jdevelops.jap.core.util.JPAUtilExpandCriteria;
+import cn.jdevelops.jap.core.util.criteria.Restrictions;
 import cn.jdevelops.jpa.server.enums.FieldName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -45,7 +47,7 @@ public interface JpaBasicsDao<T, D> extends JpaRepository<T, D>, JpaSpecificatio
     List<T> findByIdIn(List<D> id);
 
     /**
-     *  更新 根据id
+     *  更新 根据id(id必须要有值)
      * @param t 实体类型的 数据
      * @return Boolean
      * @throws Exception Exception
@@ -54,6 +56,37 @@ public interface JpaBasicsDao<T, D> extends JpaRepository<T, D>, JpaSpecificatio
         /* 跟根据ID获取需要更新的数据的 原始数据 */
         T  oidCamera = findById((D) CommUtils.getFieldValueByName(FieldName.ID.getFieldName(), t))
                 .orElse(null);
+        /*
+         *将新数据中非空字段 克隆到原始数据中 实现更新
+         * <p> oidCamera.copy(scCameraEntity); </p>
+         */
+        /* 获取method对象，其中包含方法名称和参数列表*/
+        Method setName = oidCamera.getClass().getMethod("copy", Object.class);
+        /* 执行method，t为实例对象，后面是方法参数列表；setName没有返回值 */
+        setName.invoke(oidCamera, t);
+        /* 保存克隆之后的数据  且 saveAndFlush立即生效 */
+        saveAndFlush(oidCamera);
+        return true;
+    }
+
+
+    /**
+     *  更新 根据id
+     * @param t 实体类型的 数据
+     * @param selectKey 指定唯一键 (t中必须要有selectKey的值)，e.g uuid
+     * @return Boolean
+     * @throws Exception Exception
+     */
+    default Boolean updateEntity(T t,String selectKey) throws Exception {
+        /* 跟根据ID获取需要更新的数据的 原始数据 */
+        T  oidCamera;
+        try {
+            JPAUtilExpandCriteria<T> jpaSelect = new JPAUtilExpandCriteria<>();
+            jpaSelect.add(Restrictions.eq(selectKey, CommUtils.getFieldValueByName(selectKey,t), false));
+            oidCamera = findAll(jpaSelect).get(0);
+        }catch (Exception e){
+            throw new RuntimeException("更新失败，查询数据为空",e);
+        }
         /*
          *将新数据中非空字段 克隆到原始数据中 实现更新
          * <p> oidCamera.copy(scCameraEntity); </p>
