1283366e094aa39c5326296768de1d0f71374c76
tan <1445763190@qq.com>
2022-07-24 03:53:43 +0800

    优化注释

--- jdevelops-spi/src/test/java/cn/jdevelops/spi/ExtensionLoaderTest.java
+++ jdevelops-spi/src/test/java/cn/jdevelops/spi/ExtensionLoaderTest.java
@@ -7,8 +7,10 @@ public class ExtensionLoaderTest extends TestCase {
 
 
     public void testExtensionTest(){
-        ExtensionLoader.getExtensionLoader(TestSPI.class).getJoin("testSPI1").test();
+        // name 在  META-INF.jdevelops 中定义的
         ExtensionLoader.getExtensionLoader(TestSPI.class).getJoin("testSPI2").test();
+        ExtensionLoader.getExtensionLoader(TestSPI.class).getJoin("testSPI2").test();
+        ExtensionLoader.getExtensionLoader(TestSPI.class).getJoins().forEach(TestSPI::test);
     }
 
 
