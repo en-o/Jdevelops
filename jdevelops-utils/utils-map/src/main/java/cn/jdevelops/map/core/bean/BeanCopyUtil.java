7d94206439176fac94005ade76fcfa4efc828dcb
tan <13320385140@163.com>
2023-01-08 03:14:04 +0800

    pref(bean copy): 1. 新增注释 2. 新增单元测试 3. 新增空串的处理类

--- jdevelops-utils/utils-map/src/main/java/cn/jdevelops/map/core/bean/BeanCopyUtil.java
+++ jdevelops-utils/utils-map/src/main/java/cn/jdevelops/map/core/bean/BeanCopyUtil.java
@@ -14,10 +14,11 @@ import java.util.Set;
  * 类说明   复制bean
  */
 public class BeanCopyUtil {
-
+	private static final String EMPTY_STRING = "";
 
 	/**
 	 * source中的非空属性复制到target中
+	 *  只会忽略 null,不会忽略 "" ,若要想忽略 "" , 请用 beanCopyEmpty
 	 * <pre>{@code
 	 *   public void copy(UserMsgSubscribeBean source) {
 	 *     BeanCopyUtil.beanCopy(source, this);
@@ -37,6 +38,16 @@ public class BeanCopyUtil {
 	}
 
 	/**
+	 * 特殊方法(按常理参数为"",是表示为需要将值置为"",这个方法是针对特殊时候,如前端不想做空不传的判断
+	 * 同上 beanCopy 但是这个可以忽略 value = "" 的key
+	 */
+	@SuppressWarnings("AlibabaRemoveCommentedCode")
+	public static <T> void beanCopyEmpty(T source, T target) {
+		BeanUtils.copyProperties(source, target, getNullAndEmptyPropertyNames(source));
+	}
+
+
+	/**
 	 * source中的非空属性复制到target中，但是忽略指定的属性，也就是说有些属性是不可修改的（个人业务需要）
 	 *
 	 * @param source           source
@@ -49,6 +60,13 @@ public class BeanCopyUtil {
 		BeanUtils.copyProperties(source, target, pns);
 	}
 
+
+	/**
+	 * 获取空值的key
+	 * @param source bean
+	 * @param ignoreProperties 指定一定忽略的字段
+	 * @return string of array
+	 */
 	public static String[] getNullAndIgnorePropertyNames(Object source, String... ignoreProperties) {
 		Set<String> emptyNames = getNullPropertyNameSet(source);
 		emptyNames.addAll(Arrays.asList(ignoreProperties));
@@ -56,12 +74,56 @@ public class BeanCopyUtil {
 		return emptyNames.toArray(result);
 	}
 
+
+
+
+	/**
+	 * 获取空值和空串的key
+	 * @param source bean
+	 * @return string of array
+	 */
+	public static String[] getNullAndEmptyPropertyNames(Object source) {
+		Set<String> emptyNames = getNullAndEmptyPropertyNameSet(source);
+		String[] result = new String[emptyNames.size()];
+		return emptyNames.toArray(result);
+	}
+
+
+	/**
+	 * 获取空值和空串的key
+	 * @param source bean
+	 * @return string of Set
+	 */
+	public static Set<String> getNullAndEmptyPropertyNameSet(Object source) {
+		final BeanWrapper src = new BeanWrapperImpl(source);
+		java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+		Set<String> emptyNames = new HashSet<>();
+		for (java.beans.PropertyDescriptor pd : pds) {
+			Object srcValue = src.getPropertyValue(pd.getName());
+			if (srcValue == null || EMPTY_STRING.equals(srcValue)) {
+				emptyNames.add(pd.getName());
+			}
+		}
+		return emptyNames;
+	}
+
+
+	/**
+	 * 获取空值的key
+	 * @param source bean
+	 * @return string of array
+	 */
 	public static String[] getNullPropertyNames(Object source) {
 		Set<String> emptyNames = getNullPropertyNameSet(source);
 		String[] result = new String[emptyNames.size()];
 		return emptyNames.toArray(result);
 	}
 
+	/**
+	 * 获取空值的key
+	 * @param source bean
+	 * @return string of Set
+	 */
 	public static Set<String> getNullPropertyNameSet(Object source) {
 		final BeanWrapper src = new BeanWrapperImpl(source);
 		java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
