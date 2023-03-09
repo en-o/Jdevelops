5592189f68ac46a219131d6a20b72f435ffec235
tan <13320385140@163.com>
2023-03-09 17:22:28 +0800

    tem: 查询job详情，还有点问题先提交了

--- jdevelops-quartz/spring-quartz-quick/src/main/java/cn/jdevelops/spring/quart/dao/QrtzJobDetailsDao.java
+++ jdevelops-quartz/spring-quartz-quick/src/main/java/cn/jdevelops/spring/quart/dao/QrtzJobDetailsDao.java
@@ -1,8 +1,12 @@
 package cn.jdevelops.spring.quart.dao;
 
+import cn.jdevelops.spring.quart.dao.bo.JobAndTriggerBO;
 import cn.jdevelops.spring.quart.entity.QrtzJobDetailsEntity;
 import cn.jdevelops.spring.quart.entity.key.QrtzJobDetailsUPK;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * 任务详情
@@ -13,4 +17,28 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface QrtzJobDetailsDao extends JpaRepository<QrtzJobDetailsEntity, QrtzJobDetailsUPK> {
 
+
+    /**
+     * 查询任务详情
+     * @param pageable 分页
+     * @return JobAndTriggerBO of Page
+     */
+    @Query("SELECT DISTINCT " +
+            " jd.jobDetailsUPK.jobName , " +
+            " jd.jobDetailsUPK.jobGroup , " +
+            " jd.jobClassName , " +
+            " qt.startTime, " +
+            " qt.triggerType, " +
+            " qt.triggerState, " +
+            " qt.cronTriggersUPK.triggerName , " +
+            " qt.cronTriggersUPK.triggerGroup , " +
+            " ct.cronExpression , " +
+            " ct.timeZoneId  " +
+            "FROM " +
+            " QrtzJobDetailsEntity jd " +
+            "LEFT JOIN QrtzTriggersEntity qt ON qt.cronTriggersUPK.triggerGroup = jd.jobDetailsUPK.jobGroup " +
+            "LEFT JOIN QrtzCronTriggersEntity ct ON jd.jobDetailsUPK.jobName = qt.cronTriggersUPK.triggerName " +
+            "AND qt.cronTriggersUPK.triggerName = ct.cronTriggersUPK.triggerName " +
+            "AND qt.cronTriggersUPK.triggerGroup = ct.cronTriggersUPK.triggerGroup ")
+    Page<JobAndTriggerBO> findJobAndTrigger(Pageable pageable);
 }
