ffd0b3c63396cc0a16dd539d67545704f064b6fb
tan <13320385140@163.com>
2023-04-02 13:22:26 +0800

    refactor(auto-schema): 模块

--- jdevelops-data/jdevelops-data-auto-schema/src/main/java/cn/jdevelops/schema/scan/EnableAutoSchema.java
+++ jdevelops-data/jdevelops-data-auto-schema/src/main/java/cn/jdevelops/schema/scan/EnableAutoSchema.java
@@ -2,22 +2,18 @@ package cn.jdevelops.schema.scan;
 
 import cn.jdevelops.schema.LocalDataSourceLoader;
 import cn.jdevelops.schema.properties.DataBaseProperties;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
 
 /**
- * 启动是检查库存在与否，进行自动建库
+ * 开启自动建库
  * @author tnnn
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 @Documented
-//1.把需要注入spring容器的类加入进来
 @Import({DataBaseProperties.class, LocalDataSourceLoader.class})
-//2.设置扫描路径：最好是直接把本项目所有类的共有目录放进去
-@ComponentScan("cn.jdevelops.spring.**")
 public  @interface EnableAutoSchema {
 
 }
