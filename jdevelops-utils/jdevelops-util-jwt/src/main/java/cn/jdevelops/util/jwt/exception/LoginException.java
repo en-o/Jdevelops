62ba52164d7d44dfa436d60e5429e9ce564b958d
tan <13320385140@163.com>
2023-04-20 09:26:00 +0800

    fix: LoginException.java

--- jdevelops-utils/jdevelops-util-jwt/src/main/java/cn/jdevelops/util/jwt/exception/LoginException.java
+++ jdevelops-utils/jdevelops-util-jwt/src/main/java/cn/jdevelops/util/jwt/exception/LoginException.java
@@ -1,17 +1,41 @@
 package cn.jdevelops.util.jwt.exception;
 
+import cn.jdevelops.api.exception.exception.BusinessException;
+import cn.jdevelops.api.result.emums.TokenExceptionCodeEnum;
+import cn.jdevelops.api.result.emums.UserExceptionEnum;
+
 /**
  * JwtException
+ *
  * @author tnnn
  */
-public class LoginException extends RuntimeException{
-    public LoginException(String message)
-    {
+public class LoginException extends BusinessException {
+    public LoginException(String message) {
         super(message);
     }
 
-    public LoginException(String message, Throwable cause)
-    {
+    public LoginException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    public LoginException(int code, String message, Throwable cause) {
+        super(message, cause, code);
+    }
+
+
+    public LoginException(TokenExceptionCodeEnum constantEnum) {
+        super(constantEnum.getCode(), constantEnum.getMessage());
+    }
+
+    public LoginException(TokenExceptionCodeEnum constantEnum, Throwable cause) {
+        super(constantEnum.getMessage(), cause, constantEnum.getCode());
+    }
+
+    public LoginException(UserExceptionEnum constantEnum) {
+        super(constantEnum.getCode(), constantEnum.getMessage());
+    }
+
+    public LoginException(UserExceptionEnum constantEnum, Throwable cause) {
+        super(constantEnum.getMessage(), cause, constantEnum.getCode());
     }
 }
