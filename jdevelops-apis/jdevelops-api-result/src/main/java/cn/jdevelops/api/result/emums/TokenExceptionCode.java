78a9d5de45854c3caae70026c1ab63caa6e9695e
en-o <13320385140@163.com>
2023-07-05 09:31:11 +0800

    fix: code

--- jdevelops-apis/jdevelops-api-result/src/main/java/cn/jdevelops/api/result/emums/TokenExceptionCode.java
+++ jdevelops-apis/jdevelops-api-result/src/main/java/cn/jdevelops/api/result/emums/TokenExceptionCode.java
@@ -20,23 +20,23 @@ public class TokenExceptionCode {
     /**
      * token 校验失败异常
      */
-    public static final ExceptionCode TOKEN_ERROR = new ExceptionCode(403, "登录失效，请重新登录");
+    public static final ExceptionCode TOKEN_ERROR = new ExceptionCode(401, "登录失效，请重新登录");
 
     /**
      * redis 登录失效，请重新登录（token不存在）
      */
-    public static final ExceptionCode REDIS_EXPIRED_USER = new ExceptionCode(403, "登录失效，请重新登录");
+    public static final ExceptionCode REDIS_EXPIRED_USER = new ExceptionCode(401, "登录失效，请重新登录");
 
     /**
      * 非法登录（token 更换过）
      */
-    public static final ExceptionCode REDIS_NO_USER = new ExceptionCode(403, "登录失效，请重新登录");
+    public static final ExceptionCode REDIS_NO_USER = new ExceptionCode(401, "登录失效，请重新登录");
 
 
     /**
      * 系统未授权
      */
-    public static final ExceptionCode UNAUTHENTICATED = new ExceptionCode(401, "系统未授权,请先登录");
+    public static final ExceptionCode UNAUTHENTICATED = new ExceptionCode(403, "系统未授权,禁止访问");
 
     /**
      * 授权过期
