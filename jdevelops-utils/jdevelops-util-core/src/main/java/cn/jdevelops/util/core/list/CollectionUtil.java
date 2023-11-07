df6af23c28a53d97606cec6ca216b2e004d85b4a
en-o <13320385140@163.com>
2023-11-07 13:45:06 +0800

    feat(util): list 交集

--- jdevelops-utils/jdevelops-util-core/src/main/java/cn/jdevelops/util/core/list/CollectionUtil.java
+++ jdevelops-utils/jdevelops-util-core/src/main/java/cn/jdevelops/util/core/list/CollectionUtil.java
@@ -1,5 +1,15 @@
 package cn.jdevelops.util.core.list;
 
+import cn.hutool.core.util.ObjectUtil;
+import org.apache.commons.lang3.ObjectUtils;
+
+import java.awt.*;
+import java.util.AbstractCollection;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+
 /**
  * 集合
  *
@@ -18,4 +28,73 @@ public class CollectionUtil {
     public static <T> boolean isEmpty(T[] array) {
         return array == null || array.length == 0;
     }
+
+    /**
+     * 判空
+     * Null-safe check if the specified collection is empty.
+     * <p>
+     * Null returns true.[true空]
+     * </p>
+     *
+     * @param coll  the collection to check, may be null
+     * @return true if empty or null
+     * @since 3.2
+     */
+    public static boolean isEmpty(final Collection<?> coll) {
+        return coll == null || coll.isEmpty();
+    }
+
+    /**
+     * Null-safe check if the specified collection is not empty.
+     * <p>
+     * Null returns false.
+     * </p>
+     *
+     * @param coll  the collection to check, may be null
+     * @return true if non-null and non-empty
+     * @since 3.2
+     */
+    public static boolean isNotEmpty(final Collection<?> coll) {
+        return !isEmpty(coll);
+    }
+
+    /**
+     * 根据list的长度返回 map所需的 initialCapacity
+     * @return 如果list为空返回默认为10
+     */
+    public static int initialCapacity(final Collection<?> coll){
+        if(isEmpty(coll)){
+            return 10;
+        }else {
+           return coll.size();
+        }
+    }
+
+    /**
+     * 取交集
+     * @param c1 list
+     * @param c2 list
+     * @return 交集 list [空返回空对象]
+     */
+    public static Collection<?> intersection(Collection<?> c1, Collection<?> c2){
+
+        if(isEmpty(c1)|| isEmpty(c2)){
+            return Collections.emptyList();
+        }
+        // 为了不改变传入的值，因为 rm 会造成原来的数据值被改变
+        Collection<?> temp1 = ObjectUtils.clone(c1);
+        Collection<?> temp2 = ObjectUtils.clone(c2);
+        Collection<?> result = ObjectUtils.clone(c1);
+        // 删除成功表示，有交集
+        if(temp1.removeAll(temp2) ){
+            if(result.removeAll(temp1)){
+                // 获取被删除的的交集
+                return result;
+            }
+
+        }
+        return Collections.emptyList();
+    }
+
+
 }
--- jdevelops-utils/jdevelops-util-core/src/test/java/cn/jdevelops/util/core/list/CollectionUtilTest.java
+++ jdevelops-utils/jdevelops-util-core/src/test/java/cn/jdevelops/util/core/list/CollectionUtilTest.java
@@ -2,6 +2,8 @@ package cn.jdevelops.util.core.list;
 
 import junit.framework.TestCase;
 
+import java.util.*;
+
 
 public class CollectionUtilTest extends TestCase {
 
@@ -10,5 +12,98 @@ public class CollectionUtilTest extends TestCase {
         assertTrue(CollectionUtil.isEmpty(new String[0]));
         assertFalse(CollectionUtil.isEmpty(new Integer[]{1,2}));
         assertTrue(CollectionUtil.isEmpty(new Integer[0]));
+    }
+
+    public void testTestIsEmpty() {
+        assertFalse(CollectionUtil.isEmpty(Arrays.asList("1","2")));
+        assertTrue(CollectionUtil.isEmpty(Arrays.asList()));
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        assertTrue(CollectionUtil.isEmpty(arrayList));
+        arrayList.add("12");
+        assertFalse(CollectionUtil.isEmpty(arrayList));
+
+        assertTrue(CollectionUtil.isEmpty(Collections.emptyList()));
+
+        HashSet<String> hashSet = new HashSet<>();
+        assertTrue(CollectionUtil.isEmpty(hashSet));
+        hashSet.add("ad");
+        assertFalse(CollectionUtil.isEmpty(hashSet));
+
+        assertTrue(CollectionUtil.isEmpty(Collections.emptySet()));
+    }
+
+    public void testIsNotEmpty() {
+
+        assertTrue(CollectionUtil.isNotEmpty(Arrays.asList("1","2")));
+        assertFalse(CollectionUtil.isNotEmpty(Arrays.asList()));
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        assertFalse(CollectionUtil.isNotEmpty(arrayList));
+        arrayList.add("12");
+        assertTrue(CollectionUtil.isNotEmpty(arrayList));
+
+        assertFalse(CollectionUtil.isNotEmpty(Collections.emptyList()));
+
+        HashSet<String> hashSet = new HashSet<>();
+        assertFalse(CollectionUtil.isNotEmpty(hashSet));
+        hashSet.add("ad");
+        assertTrue(CollectionUtil.isNotEmpty(hashSet));
+
+        assertFalse(CollectionUtil.isNotEmpty(Collections.emptySet()));
+    }
+
+    public void testInitialCapacity() {
+        assertEquals(CollectionUtil.initialCapacity(Collections.emptySet()),10);
+        assertEquals(CollectionUtil.initialCapacity(Collections.emptyList()),10);
+        assertEquals(CollectionUtil.initialCapacity(Arrays.asList("1","2")),2);
+    }
+
+    public void testIntersection() {
+        ArrayList<String> c1 = new ArrayList<>();
+        ArrayList<String> c2 = new ArrayList<>();
+        assertEquals(CollectionUtil.intersection(c1,c2).size(),0);
+        c1.add("12");
+        c1.add("23");
+        c1.add("4");
+        assertEquals(CollectionUtil.intersection(c1,c2).size(),0);
+        c2.add("3");
+        c2.add("5");
+        assertEquals(CollectionUtil.intersection(c1,c2).size(),0);
+        c2.add("4");
+        c2.add("12");
+        //  c1 跟 c2 的交集为 4，12
+        Collection<?> intersection = CollectionUtil.intersection(c1, c2);
+        assertEquals(intersection.toString(),"[12, 4]");
+        // 测试 前后数据量反转测试
+        Collection<?> reIntersection = CollectionUtil.intersection(c2, c1);
+        // 顺序
+        assertEquals(reIntersection.toString(),"[4, 12]");
+    }
+
+    /**
+     * bean的话必须重写 equals
+     * @see TestBean#equals(Object)
+     */
+    public void testIntersectionBean() {
+        ArrayList<TestBean> c1 = new ArrayList<>();
+        ArrayList<TestBean> c2 = new ArrayList<>();
+        assertEquals(CollectionUtil.intersection(c1,c2).size(),0);
+        c1.add(new TestBean("tan",1));
+        c1.add(new TestBean("ning",2));
+        c1.add(new TestBean("tanning",3));
+        assertEquals(CollectionUtil.intersection(c1,c2).size(),0);
+        c2.add(new TestBean("ning",4));
+        c2.add(new TestBean("admin",1));
+        assertEquals(CollectionUtil.intersection(c1,c2).size(),0);
+        c2.add(new TestBean("ning",2));
+        c2.add(new TestBean("tan",1));
+        //  c1 跟 c2 的交集为 tan:1，ning:2
+        Collection<?> intersection = CollectionUtil.intersection(c1, c2);
+        assertEquals(intersection.toString(),"[TestBean{name='tan', sex=1}, TestBean{name='ning', sex=2}]");
+        // 测试 前后数据量反转测试
+        Collection<?> reIntersection = CollectionUtil.intersection(c2, c1);
+        // 顺序
+        assertEquals(reIntersection.toString(),"[TestBean{name='ning', sex=2}, TestBean{name='tan', sex=1}]");
     }
 }
