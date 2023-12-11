db6a3c57d0c26d7c8fc6fcc0e1645310626f8499
en-o <13320385140@163.com>
2023-12-11 09:24:58 +0800

    style: 注释

--- jdevelops-data/jdevelops-data-es/src/main/java/cn/jdevelops/data/es/config/ElasticProperties.java
+++ jdevelops-data/jdevelops-data-es/src/main/java/cn/jdevelops/data/es/config/ElasticProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * ES基础配置
+ * ES mapping 构建用基础配置
  *
  * @author <a href="https://tannn.cn/">tan</a>
  * @date 2023/9/25 11:39
@@ -13,7 +13,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "jdevelops.elasticsearch")
 public class ElasticProperties {
     /**
-     * 扫描包路径[包含子路径] e.g com.text [他会扫描这个路径下所有的类包括子路径的类]
+     * 扫描[实体类包]包路径[包含子路径] e.g com.text [他会扫描这个路径下所有的类包括子路径的类]
      */
     String basePackage;
 
