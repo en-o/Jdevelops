d4b6ccf9930e5efbc2d3ae1957c70f8cf548738f
en-o <13320385140@163.com>
2024-01-08 13:31:30 +0800

    feat: authorization error处理

---
+++ jdevelops-utils/jdevelops-util-authorization-error/src/main/java/cn/jdevelops/util/authorization/error/UnAccessDeniedHandler.java
@@ -0,0 +1,31 @@
+package cn.jdevelops.util.authorization.error;
+
+import cn.jdevelops.api.result.emums.ExceptionCode;
+import cn.jdevelops.api.result.emums.TokenExceptionCode;
+import cn.jdevelops.util.authorization.error.respone.ResponseUtil;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.oauth2.server.resource.authentication.AbstractOAuth2TokenAuthenticationToken;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 鉴权失败处理器
+ */
+public class UnAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        ExceptionCode unauthenticated = TokenExceptionCode.UNAUTHENTICATED;
+        if(request.getUserPrincipal() instanceof AbstractOAuth2TokenAuthenticationToken){
+            ResponseUtil.exceptionResponse(response,unauthenticated);
+        }else {
+            ResponseUtil.exceptionResponse(response, new ExceptionCode(unauthenticated.getCode(),
+                    ResponseUtil.getExceptionMessage(accessDeniedException)));
+        }
+    }
+
+}
