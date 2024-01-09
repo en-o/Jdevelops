27684454138685aece784ed0d6150a1843176ecf
en-o <13320385140@163.com>
2024-01-09 13:37:55 +0800

    feat: authorization 客户端配置

---
+++ jdevelops-framework/jdevelops-sboot-authentication/jdevelops-sboot-authentication-sas/jdevelops-sboot-authentication-sas-client/src/main/java/cn/jdevelops/sboot/authentication/sas/client/config/ClientScanBean.java
@@ -0,0 +1,37 @@
+package cn.jdevelops.sboot.authentication.sas.client.config;
+
+import cn.jdevelops.sboot.authentication.sas.client.core.SasAuthorizeHttpRequests;
+import cn.jdevelops.sboot.authentication.sas.client.core.WebSecurityConfig;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 扫描
+ *
+ * @author <a href="https://tannn.cn/">tan</a>
+ * @date 2024/1/9 13:23
+ */
+@ConditionalOnWebApplication
+public class ClientScanBean {
+
+    /**
+     * 加载基础配置
+     */
+    @ConditionalOnMissingBean(SasAuthorizeHttpRequests.class)
+    @Bean
+    public SasAuthorizeHttpRequests sasAuthorizeHttpRequests(){
+        return new SasAuthorizeHttpRequests();
+    }
+
+
+    /**
+     * WebSecurityConfig
+     */
+    @ConditionalOnMissingBean(WebSecurityConfig.class)
+    @Bean
+    public WebSecurityConfig webSecurityConfig(SasAuthorizeHttpRequests sasAuthorizeHttpRequests){
+        return new WebSecurityConfig(sasAuthorizeHttpRequests);
+    }
+
+}
