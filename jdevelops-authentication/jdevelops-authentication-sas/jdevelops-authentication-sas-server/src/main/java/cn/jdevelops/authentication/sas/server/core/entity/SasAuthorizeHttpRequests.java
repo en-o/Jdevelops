4c81d23b476d489cd4839c369ce484af82feccac
en-o <13320385140@163.com>
2024-01-25 15:33:55 +0800

    fix: 放行客户端注册

--- jdevelops-authentication/jdevelops-authentication-sas/jdevelops-authentication-sas-server/src/main/java/cn/jdevelops/authentication/sas/server/core/entity/SasAuthorizeHttpRequests.java
+++ jdevelops-authentication/jdevelops-authentication-sas/jdevelops-authentication-sas-server/src/main/java/cn/jdevelops/authentication/sas/server/core/entity/SasAuthorizeHttpRequests.java
@@ -26,6 +26,7 @@ public class SasAuthorizeHttpRequests {
         mvcMatchers.add("/assets/**");
         mvcMatchers.add("/webjars/**");
         mvcMatchers.add("/page/login");
+        mvcMatchers.add("/api/addClient");
         return mvcMatchers;
     }
 
