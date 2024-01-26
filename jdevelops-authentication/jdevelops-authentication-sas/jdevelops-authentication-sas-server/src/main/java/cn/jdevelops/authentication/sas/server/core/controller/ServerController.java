bb20bfad8265d4f347c38ab58c80f0b2f5bcf235
en-o <13320385140@163.com>
2024-01-26 11:04:46 +0800

    fix: 空处理

--- jdevelops-authentication/jdevelops-authentication-sas/jdevelops-authentication-sas-server/src/main/java/cn/jdevelops/authentication/sas/server/core/controller/ServerController.java
+++ jdevelops-authentication/jdevelops-authentication-sas/jdevelops-authentication-sas-server/src/main/java/cn/jdevelops/authentication/sas/server/core/controller/ServerController.java
@@ -51,11 +51,11 @@ public class ServerController {
     public String addClient(@RequestBody @Valid CustomRegisteredClient client) {
         // JWT（Json Web Token）的配置项：TTL、是否复用refreshToken等等
         TokenSettings tokenSettings = TokenSettings.builder()
-                // 令牌存活时间：2小时
+                // 令牌存活时间：n小时
                 .accessTokenTimeToLive(Duration.ofHours(sasProperties.getToken().getAccessTokenTimeToLive()))
                 // 令牌可以刷新，重新获取
                 .reuseRefreshTokens(sasProperties.getToken().getReuseRefreshTokens())
-                // 刷新时间：30天（30天内当令牌过期时，可以用刷新令牌重新申请新令牌，不需要再认证）
+                // 刷新时间：n天（n天内当令牌过期时，可以用刷新令牌重新申请新令牌，不需要再认证）
                 .refreshTokenTimeToLive(Duration.ofDays(sasProperties.getToken().getRefreshTokenTimeToLive()))
                 .build();
 
@@ -65,7 +65,7 @@ public class ServerController {
                 .requireAuthorizationConsent(true)
                 .build();
 
-        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+        RegisteredClient.Builder builder = RegisteredClient.withId(UUID.randomUUID().toString())
                 // 客户端ID和密码
                 .clientId(client.getClientId())
                 .clientSecret(passwordEncoder.encode(client.getClientSecret()))
@@ -73,44 +73,33 @@ public class ServerController {
 //                .clientSecret("{noop}secret")
                 .clientName(client.getClientName())
                 // 授权方法
-                .clientAuthenticationMethods(clientAuthenticationMethods -> {
-                    clientAuthenticationMethods.addAll(client.getClientAuthenticationMethods());
-                })
-                .authorizationGrantTypes(grantTypes -> {
-                    grantTypes.addAll(client.getAuthorizationGrantTypes());
-                })
-//                // 授权模式（授权码模式）
-//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-//                // 刷新令牌（授权码模式）
-//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-//                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                // 回调地址：授权服务器向当前客户端响应时调用下面地址, 不在此列的地址将被拒绝, 只能使用IP或域名，不能使用 localhost
-//                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
-//                .redirectUri("http://127.0.0.1:8080/authorized")
-                // 将上面的redirectUri地址注释掉，改成下面的地址，是因为我们暂时还没有客户端服务，以免重定向跳转错误导致接收不到授权码
-                .redirectUris(uri -> {
-                    uri.addAll(client.getRedirectUris());
-                })
-                // 设置客户端权限范围
-                // OIDC 支持
-                .scopes( scope -> {
-//                    授权范围（当前客户端的授权范围）
-                    scope.addAll(client.getScopes());
-                })
-                .clientSecretExpiresAt(
-                        null == client.getClientSecretExpiresAt()
-                                ?null: LocalDate.parse(client.getClientSecretExpiresAt())
-                                .atStartOfDay(ZoneId.systemDefault())
-                                .toInstant()
+                .clientAuthenticationMethods(clientAuthenticationMethods ->
+                        clientAuthenticationMethods.addAll(client.getClientAuthenticationMethods())
                 )
+                .authorizationGrantTypes(grantTypes -> grantTypes.addAll(client.getAuthorizationGrantTypes()))
+                // 设置客户端权限范围（当前客户端的授权范围）
+                .scopes(scope -> scope.addAll(client.getScopes()))
                 // JWT（Json Web Token）配置项
                 .tokenSettings(tokenSettings)
                 // 客户端配置项
-                .clientSettings(clientSettings)
-                .build();
+                .clientSettings(clientSettings);
+
+        // 客户端回调地址
+        if(client.getRedirectUris() != null){
+            builder.redirectUris(uri -> uri.addAll(client.getRedirectUris()));
+        }
+        // 客户端过期时间，为空不设置，不设置等于不过期
+        if(null != client.getClientSecretExpiresAt()){
+            builder.clientSecretExpiresAt(
+                    LocalDate.parse(client.getClientSecretExpiresAt())
+                            .atStartOfDay(ZoneId.systemDefault())
+                            .toInstant()
+                );
+        }
         try {
+            RegisteredClient registeredClient =builder.build();
             registeredClientRepository.save(registeredClient);
-        }catch (IllegalArgumentException e){
+        }catch (Exception e){
             if(e.getMessage().contains("Registered client must be unique")){
                log.error("客户端已存在不要重复创建");
             }
