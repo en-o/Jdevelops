5872f7b1ca710d322af68c72ff73bfcd7d16c866
en-o <13320385140@163.com>
2024-05-09 17:11:28 +0800

    import

--- jdevelops-apis/jdevelops-apis-exception/src/main/java/cn/tannn/jdevelops/exception/config/ExceptionConfig.java
+++ jdevelops-apis/jdevelops-apis-exception/src/main/java/cn/tannn/jdevelops/exception/config/ExceptionConfig.java
@@ -1,7 +1,6 @@
 package cn.tannn.jdevelops.exception.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 
 /**
