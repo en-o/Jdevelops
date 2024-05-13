5e0f983ce00430806dc1d5d56638177d8ec7529b
en-o <13320385140@163.com>
2024-05-13 17:00:24 +0800

    UuidStrCustomGenerator

---
+++ jdevelops-dals/jdevelops-dals-jpa/src/main/java/cn/tannn/jdevelops/jpa/generator/UuidStrCustomGenerator.java
@@ -0,0 +1,32 @@
+package cn.tannn.jdevelops.jpa.generator;
+
+import cn.tannn.jdevelops.result.utils.UUIDUtils;
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
+
+import java.io.Serializable;
+
+/**
+ *
+ * 自定义主键生成策略 {@link UUIDUtils#generateShortUuid()}
+ * <code>
+ *     @Id
+ *     @GeneratedValue(strategy = GenerationType.AUTO, generator = "uuidStrCustomGenerator")
+ *     @GenericGenerator(name = "uuidStrCustomGenerator", strategy = "cn.jdevelops.data.jap.generator.UuidStrCustomGenerator")
+ *     @Column(columnDefinition="bigint")
+ *     @Comment("uuid")
+ *     @JsonSerialize(using = ToStringSerializer.class)
+ *     private Long id;
+ * </code>
+ *
+ * <p>https://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#identifiers-generators-GenericGenerator
+ * @author <a href="https://tannn.cn/">tan</a>
+ * @date 2024/4/15 9:42
+ */
+public class UuidStrCustomGenerator implements IdentifierGenerator {
+    @Override
+    public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
+        return UUIDUtils.getInstance().generateShortUuid();
+    }
+}
