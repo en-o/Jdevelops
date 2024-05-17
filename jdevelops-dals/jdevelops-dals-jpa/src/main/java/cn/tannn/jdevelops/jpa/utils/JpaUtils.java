e8b21763d11f7a860edf7175bf054d06d5101e45
en-o <13320385140@163.com>
2024-05-17 11:51:04 +0800

    fix(jpa): combinePredicates 的连接符拼接

--- jdevelops-dals/jdevelops-dals-jpa/src/main/java/cn/tannn/jdevelops/jpa/utils/JpaUtils.java
+++ jdevelops-dals/jdevelops-dals-jpa/src/main/java/cn/tannn/jdevelops/jpa/utils/JpaUtils.java
@@ -314,10 +314,23 @@ public class JpaUtils {
         Predicate result = predicates.get(0);
         for (int i = 1; i < predicates.size(); i++) {
             Predicate current = predicates.get(i);
-            if (result.getOperator() == Predicate.BooleanOperator.AND && current.getOperator() == Predicate.BooleanOperator.AND) {
+
+            if (result.getOperator() == Predicate.BooleanOperator.AND
+                    && current.getOperator() == Predicate.BooleanOperator.AND) {
+                // 前后都是and
                 result = criteriaBuilder.and(result, current);
-            } else if (result.getOperator() == Predicate.BooleanOperator.OR && current.getOperator() == Predicate.BooleanOperator.OR) {
+            } else if (result.getOperator() == Predicate.BooleanOperator.OR
+                    && current.getOperator() == Predicate.BooleanOperator.OR) {
+                // 前后都是or
                 result = criteriaBuilder.or(result, current);
+            } else if (result.getOperator() == Predicate.BooleanOperator.OR
+                    && current.getOperator() == Predicate.BooleanOperator.AND) {
+                // 前面 or 后面 and,  result, current 用result的or连接 并重置为current的and
+                result = criteriaBuilder.and(criteriaBuilder.or(result, current));
+            } else if (result.getOperator() == Predicate.BooleanOperator.AND
+                    && current.getOperator() == Predicate.BooleanOperator.OR) {
+                // 跟第三个判断反着来
+                result = criteriaBuilder.or(criteriaBuilder.and(result, current));
             } else {
                 // 默认and
                 result = criteriaBuilder.and(result, current);
