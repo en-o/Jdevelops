489b96c2004c2a8c31c6d3182818e1dea2c2e267
en-o <13320385140@163.com>
2024-05-27 13:22:03 +0800

    test[time]: IMPORT

--- jdevelops-utils/jdevelops-utils-time/src/test/java/cn/tannn/jdevelops/utils/time/TimeUtilTest.java
+++ jdevelops-utils/jdevelops-utils-time/src/test/java/cn/tannn/jdevelops/utils/time/TimeUtilTest.java
@@ -12,7 +12,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class TimeUtilTest {
 
--- jdevelops-utils/jdevelops-utils-time/src/test/java/cn/tannn/jdevelops/utils/time/TimeVerifyUtilTest.java
+++ jdevelops-utils/jdevelops-utils-time/src/test/java/cn/tannn/jdevelops/utils/time/TimeVerifyUtilTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TimeVerifyUtilTest {
 
