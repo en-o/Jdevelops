29b0504fa6518024778d08159366189e48282011
en-o <13320385140@163.com>
2024-06-05 09:53:24 +0800

    build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+cd jdevelops-build
+mvn clean package install | tee -a build.log
+
+cd ../jdevelops-dependencies
+mvn clean package install | tee -a dependencies.log
+cd ../jdevelops-parent
+mvn clean package install | tee -a parent.log
+cd ../jdevelops-annotations
+mvn clean package install | tee -a annotations.log
+cd ../jdevelops-apis
+mvn clean package install | tee -a apis.log
+cd ../jdevelops-utils
+mvn clean package install | tee -a utils.log
+cd ../jdevelops-webs
+mvn clean package install | tee -a webs.log
+cd ../jdevelops-apis
+mvn clean package install | tee -a apis.log
+cd ../jdevelops-dals
+mvn clean package install | tee -a dals.log
+cd ../jdevelops-delays
+mvn clean package install | tee -a delays.log
