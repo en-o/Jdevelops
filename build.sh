728a9d0e312ff03d5f7605eea8ccddaa1e638e1c
en-o <13320385140@163.com>
2024-06-05 09:54:48 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
+echo  " ==============当前maven环境 =============="
+echo  $(mvn -v)
+echo  " ==============当前maven环境 =============="
+
 
 cd jdevelops-build
 mvn clean package install | tee -a build.log
 
 cd ../jdevelops-dependencies
 mvn clean package install | tee -a dependencies.log
+
 cd ../jdevelops-parent
 mvn clean package install | tee -a parent.log
+
 cd ../jdevelops-annotations
 mvn clean package install | tee -a annotations.log
+
 cd ../jdevelops-apis
 mvn clean package install | tee -a apis.log
+
 cd ../jdevelops-utils
 mvn clean package install | tee -a utils.log
+
 cd ../jdevelops-webs
 mvn clean package install | tee -a webs.log
+
 cd ../jdevelops-apis
 mvn clean package install | tee -a apis.log
+
 cd ../jdevelops-dals
 mvn clean package install | tee -a dals.log
+
 cd ../jdevelops-delays
 mvn clean package install | tee -a delays.log
+
+
