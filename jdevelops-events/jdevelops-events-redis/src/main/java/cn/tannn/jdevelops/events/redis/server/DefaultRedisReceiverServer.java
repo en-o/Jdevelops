13b7f6b6e92f53de73d5d8bcd4835cb545dcf864
en-o <13320385140@163.com>
2024-06-14 14:03:14 +0800

    redis 事件

---
+++ jdevelops-events/jdevelops-events-redis/src/main/java/cn/tannn/jdevelops/events/redis/server/DefaultRedisReceiverServer.java
@@ -0,0 +1,37 @@
+package cn.tannn.jdevelops.events.redis.server;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.data.redis.connection.Message;
+
+/**
+ * 消息监听数据展示实现了
+ *
+ * @author tn
+ * @date 2020-09-11 10:36
+ */
+@ConditionalOnMissingBean(RedisReceiverServer.class)
+public class DefaultRedisReceiverServer implements RedisReceiverServer<String> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultRedisReceiverServer.class);
+
+    /**
+     * 消息接收
+     *
+     * @param message message
+     * @return String
+     */
+    @Override
+    public String pubMessage(Message message) {
+        LOG.info("\n----------------------------------------------------------\n\t" +
+                 "redis监听频道 {} 的消息\n\t" +
+                 "消息体：{}\n" +
+                 "----------------------------------------------------------"
+                , new String(message.getChannel())
+                , new String(message.getBody()));
+        return message.toString();
+    }
+}
