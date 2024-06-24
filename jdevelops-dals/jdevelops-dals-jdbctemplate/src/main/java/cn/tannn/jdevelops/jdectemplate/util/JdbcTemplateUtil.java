e28cf9a311447a76f9cdb70333e49502a1e09c16
en-o <13320385140@163.com>
2024-06-24 16:49:26 +0800

    fix[dals][jdbcTemplate]:  新增rowMapper

--- jdevelops-dals/jdevelops-dals-jdbctemplate/src/main/java/cn/tannn/jdevelops/jdectemplate/util/JdbcTemplateUtil.java
+++ jdevelops-dals/jdevelops-dals-jdbctemplate/src/main/java/cn/tannn/jdevelops/jdectemplate/util/JdbcTemplateUtil.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.SingleColumnRowMapper;
 
 import java.util.List;
 
@@ -19,18 +21,19 @@ public class JdbcTemplateUtil {
 
     /**
      * 返回值为 String, Integer 基本类型时使用的方法
-     * @param jdbcTemplate jdbcTemplate
-     * @param resultRawType     返回的类型
-     * @param resolver sql
-     * @param resultActualType    返回的具体类型
+     *
+     * @param jdbcTemplate     jdbcTemplate
+     * @param resultRawType    返回的类型
+     * @param resolver         sql
+     * @param resultActualType 返回的具体类型
      * @return Object 数据
      */
     public static Object getJdbcTemplateSql(JdbcTemplate jdbcTemplate,
-                                                           String resultRawType,
-                                                           Object resolver,
-                                                           String resultActualType ) throws ClassNotFoundException {
+                                            String resultRawType,
+                                            Object resolver,
+                                            String resultActualType) throws ClassNotFoundException {
         String sql = resolver.toString();
-        LOG.debug("jdbctemplate ========> sql {}",sql);
+        LOG.debug("jdbctemplate ========> sql {}", sql);
         if (resultRawType.equals(String.class.getName())
             || resultRawType.equals(Integer.class.getName())) {
             return getJdbcTemplateSqlContextBaseType(jdbcTemplate,
@@ -43,24 +46,23 @@ public class JdbcTemplateUtil {
 
     /**
      * 返回值为 String, Integer 基本类型时使用的方法
-     * @param jdbcTemplate jdbcTemplate
-     * @param resultRawType     返回的类型
-     * @param sql sql
-     * @param resultActualType    返回的具体类型
+     *
+     * @param jdbcTemplate     jdbcTemplate
+     * @param resultRawType    返回的类型
+     * @param sql              sql
+     * @param resultActualType 返回的具体类型
      * @return Object 数据
      */
     public static Object getJdbcTemplateSqlContextBaseType(JdbcTemplate jdbcTemplate,
                                                            String resultRawType,
                                                            String sql,
-                                                           String resultActualType ) throws ClassNotFoundException {
+                                                           String resultActualType) throws ClassNotFoundException {
         Class<?> rvt = Class.forName(resultRawType);
         Class<?> query = Class.forName(resultActualType);
-        if (rvt.getName().equals(List.class.getName())) {
-            return jdbcTemplate.queryForList(sql,
-                    query);
+        if (rvt.isAssignableFrom(List.class)) {
+            return jdbcTemplate.queryForList(sql, query);
         } else {
-            return jdbcTemplate.queryForObject(sql,
-                    query);
+            return jdbcTemplate.queryForObject(sql, query);
         }
     }
 
@@ -68,26 +70,29 @@ public class JdbcTemplateUtil {
     /**
      * 返回值为Bean, Map 型时使用的方法
      *
-     * @param jdbcTemplate jdbcTemplate
-     * @param resultRawType     返回的类型
-     * @param sql sql
-     * @param resultActualType    返回的具体类型
+     * @param jdbcTemplate     jdbcTemplate
+     * @param resultRawType    返回的类型
+     * @param sql              sql
+     * @param resultActualType 返回的具体类型
      * @return Object 数据
      */
     public static Object getJdbcTemplateSqlContextMapType(JdbcTemplate jdbcTemplate
-            ,  String resultRawType
+            , String resultRawType
             , String sql
-            , String resultActualType ) throws ClassNotFoundException {
+            , String resultActualType) throws ClassNotFoundException {
         Class<?> rvt = Class.forName(resultRawType);
-        Class<?> query = Class.forName(resultActualType);
-        BeanPropertyRowMapper beanPropertyRowMapper = new BeanPropertyRowMapper<>(query);
-        if (rvt.getName().equals(List.class.getName())) {
-            return jdbcTemplate.query(sql,
-                    beanPropertyRowMapper);
-
+        if (rvt.isAssignableFrom(List.class)) {
+            return jdbcTemplate.query(sql, rowMapper(resultActualType));
         } else {
-            return jdbcTemplate.queryForObject(sql,
-                    beanPropertyRowMapper);
+            return jdbcTemplate.queryForObject(sql, rowMapper(resultActualType));
+        }
+    }
+
+    private static RowMapper<?> rowMapper(String resultActualType) throws ClassNotFoundException {
+        if (resultActualType.equals(Integer.class.getName())) {
+            return new SingleColumnRowMapper<>(Integer.class);
+        } else {
+            return new BeanPropertyRowMapper<>(Class.forName(resultActualType));
         }
     }
 }
