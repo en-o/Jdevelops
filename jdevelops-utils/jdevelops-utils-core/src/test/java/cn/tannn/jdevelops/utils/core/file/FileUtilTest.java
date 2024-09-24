a3ffe956ccd09040d8a7e4ca5b24746a830d2a77
tan <13320385140@163.com>
2024-09-24 10:44:09 +0800

    import

--- jdevelops-utils/jdevelops-utils-core/src/test/java/cn/tannn/jdevelops/utils/core/file/FileUtilTest.java
+++ jdevelops-utils/jdevelops-utils-core/src/test/java/cn/tannn/jdevelops/utils/core/file/FileUtilTest.java
@@ -2,7 +2,7 @@ package cn.tannn.jdevelops.utils.core.file;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FileUtilTest {
 
