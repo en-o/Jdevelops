206a257cc66fe9bad239923f12fc5210af8dea7f
tan <13320385140@163.com>
2024-11-06 13:45:41 +0800

    concatHead

--- jdevelops-utils/jdevelops-utils-excel/src/main/java/cn/tannn/jdevelops/util/excel/ExcelService.java
+++ jdevelops-utils/jdevelops-utils-excel/src/main/java/cn/tannn/jdevelops/util/excel/ExcelService.java
@@ -18,27 +18,41 @@ public class ExcelService {
     /**
      * 请假状态
      */
-    public static List<String> LEAVE_STATE = Arrays.asList("请假","正常");
+    public static List<String> LEAVE_STATE = Arrays.asList("请假", "正常");
     /**
      * 用户类型
      */
-    public static List<String> SCHOOL_STATE =  Arrays.asList("本校","校外");
+    public static List<String> SCHOOL_STATE = Arrays.asList("本校", "校外");
 
 
     /**
      * 下拉数据
      * <code>
-           <p> EasyExcelFactory.write(responseHeader.getOutputStream(), Bean.class)
-           <p> .registerWriteHandler(ExcelService.dropDownListLeaveState(6,LEAVE_STATE))
+     * <p> EasyExcelFactory.write(responseHeader.getOutputStream(), Bean.class)
+     * <p> .registerWriteHandler(ExcelService.dropDownListLeaveState(6,LEAVE_STATE))
      * </code>
-     * @param index  需要下拉数据的表头下标
+     *
+     * @param index     需要下拉数据的表头下标
      * @param menuItems 列表数据
      * @return CellMenu
      */
-    public static CellMenu dropDownList(Integer index,List<String> menuItems) {
+    public static CellMenu dropDownList(Integer index, List<String> menuItems) {
         // 设置下拉
-        HeaderMenuData headerMenuData = new HeaderMenuData(index, menuItems );
+        HeaderMenuData headerMenuData = new HeaderMenuData(index, menuItems);
         return new CellMenu(headerMenuData);
+    }
+
+
+    /**
+     * 填写须知
+     *
+     * @param remake      第一行[表说明]
+     * @param headContent 第二行[表字段]
+     * @return concatHead
+     */
+    private static List<String> concatHead(String remake, List<String> headContent) {
+        headContent.add(0, remake);
+        return headContent;
     }
 
 
