e4fe7f21d4569f06a978a0468435327331b0be3f
tan <13320385140@163.com>
2024-11-29 16:54:23 +0800

    xx

--- jdevelops-apis/jdevelops-apis-knife4j/src/main/java/cn/tannn/jdevelops/knife4j/domain/SwaggerProperties.java
+++ jdevelops-apis/jdevelops-apis-knife4j/src/main/java/cn/tannn/jdevelops/knife4j/domain/SwaggerProperties.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static cn.tannn.jdevelops.knife4j.core.constant.PublicConstant.SWAGGER_HEADER_HANDER;
 
