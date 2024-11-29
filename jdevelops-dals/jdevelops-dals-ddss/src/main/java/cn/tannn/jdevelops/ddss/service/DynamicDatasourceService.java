ba6fe346182dbc57d3e405596cfb06296703f035
tan <13320385140@163.com>
2024-11-29 16:37:49 +0800

    import

--- jdevelops-dals/jdevelops-dals-ddss/src/main/java/cn/tannn/jdevelops/ddss/service/DynamicDatasourceService.java
+++ jdevelops-dals/jdevelops-dals-ddss/src/main/java/cn/tannn/jdevelops/ddss/service/DynamicDatasourceService.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.security.InvalidKeyException;
 import java.util.List;
