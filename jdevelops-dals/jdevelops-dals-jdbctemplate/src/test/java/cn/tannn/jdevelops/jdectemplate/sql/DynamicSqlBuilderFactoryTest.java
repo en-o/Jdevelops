8c893e6f3df70205123d06412e2b573378bf78df
tan <13320385140@163.com>
2025-07-17 15:57:00 +0800

    动态SQL构建器工厂

---
+++ jdevelops-dals/jdevelops-dals-jdbctemplate/src/test/java/cn/tannn/jdevelops/jdectemplate/sql/DynamicSqlBuilderFactoryTest.java
@@ -0,0 +1,44 @@
+package cn.tannn.jdevelops.jdectemplate.sql;
+
+import cn.tannn.jdevelops.jdectemplate.sql.pojo.AccountQueryExample;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.DataClassRowMapper;
+
+import java.util.Arrays;
+
+/**
+ * @author <a href="https://t.tannn.cn/">tan</a>
+ * @version V1.0
+ * @date 2025/7/17 15:51
+ */
+public class DynamicSqlBuilderFactoryTest {
+
+    @Test
+    public void example() {
+        // 创建查询对象
+        AccountQueryExample query = new AccountQueryExample();
+        query.setLoginName("admin");
+        query.setName("张三");
+        query.setStatus(Arrays.asList(1, 2));
+        query.setPageIndex(0);
+        query.setPageSize(10);
+
+        // 构建SQL
+        DynamicSqlBuilder builder = DynamicSqlBuilderFactory.buildJdbc(query);
+        System.out.println(builder.getSql());
+        System.out.println(builder.getNativeSql());
+//        // 执行查询
+//        List<Account> accounts = namedParameterJdbcTemplate.query(
+//                builder.getSql(),
+//                builder.getNamedParams(),
+//                new DataClassRowMapper<>(Account.class)
+//        );
+//
+//        // 获取总数
+//        Long total = namedParameterJdbcTemplate.queryForObject(
+//                builder.buildCountSql().getSql(),
+//                builder.buildCountSql().getNamedParams(),
+//                Long.class
+//        );
+    }
+}
---
+++ jdevelops-dals/jdevelops-dals-jdbctemplate/src/test/java/cn/tannn/jdevelops/jdectemplate/sql/pojo/AccountQueryExample.java
@@ -0,0 +1,133 @@
+package cn.tannn.jdevelops.jdectemplate.sql.pojo;
+
+
+import cn.tannn.jdevelops.jdectemplate.annotations.*;
+import cn.tannn.jdevelops.jdectemplate.enums.JoinType;
+import cn.tannn.jdevelops.jdectemplate.enums.NullHandleStrategy;
+import cn.tannn.jdevelops.jdectemplate.enums.PageType;
+import cn.tannn.jdevelops.jdectemplate.enums.QueryType;
+
+import java.util.List;
+
+/**
+ * 账户查询对象示例
+ */
+@SqlTable(
+    name = "tb_account",
+    alias = "a",
+    joins = {
+        @SqlJoin(
+            type = JoinType.LEFT,
+            table = "tb_account_sensitive",
+            alias = "b",
+            condition = "b.user_id = a.id"
+        )
+    }
+)
+@SqlOrderBy("a.id DESC")
+public class AccountQueryExample {
+
+    @SqlColumn(name = "login_name", queryType = QueryType.EQ)
+    private String loginName;
+
+    @SqlColumn(name = "name", queryType = QueryType.LIKE, nullStrategy = NullHandleStrategy.IGNORE)
+    private String name;
+
+    @SqlColumn(name = "gender", queryType = QueryType.EQ)
+    private Integer gender;
+
+    @SqlColumn(name = "status", queryType = QueryType.IN)
+    private List<Integer> status;
+
+    @SqlColumn(name = "create_time", queryType = QueryType.GE, paramName = "createTimeStart")
+    private String createTimeStart;
+
+    @SqlColumn(name = "create_time", queryType = QueryType.LE, paramName = "createTimeEnd")
+    private String createTimeEnd;
+
+    @SqlColumn(name = "masked_phone", tableAlias = "b", queryType = QueryType.LIKE)
+    private String phone;
+
+    @SqlPage(type = PageType.PAGE_INDEX)
+    private Integer pageIndex;
+
+    @SqlPage(type = PageType.PAGE_SIZE)
+    private Integer pageSize;
+
+    // 或者使用分页对象
+//     @SqlPage(type = PageType.PAGE_INFO)
+//     private PageInfo pageInfo;
+
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public List<Integer> getStatus() {
+        return status;
+    }
+
+    public void setStatus(List<Integer> status) {
+        this.status = status;
+    }
+
+    public String getCreateTimeStart() {
+        return createTimeStart;
+    }
+
+    public void setCreateTimeStart(String createTimeStart) {
+        this.createTimeStart = createTimeStart;
+    }
+
+    public String getCreateTimeEnd() {
+        return createTimeEnd;
+    }
+
+    public void setCreateTimeEnd(String createTimeEnd) {
+        this.createTimeEnd = createTimeEnd;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getPageIndex() {
+        return pageIndex;
+    }
+
+    public void setPageIndex(Integer pageIndex) {
+        this.pageIndex = pageIndex;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+}
