38c96199723b8f40ece43e7b53520366b6b2d99d
tan <13320385140@163.com>
2025-07-21 13:44:30 +0800

    fix:[redis] CustomCacheConfig

--- jdevelops-dals/jdevelops-dals-redis/src/main/java/cn/tannn/jdevelops/redis/CustomCacheConfig.java
+++ jdevelops-dals/jdevelops-dals-redis/src/main/java/cn/tannn/jdevelops/redis/CustomCacheConfig.java
@@ -1,22 +1,11 @@
 package cn.tannn.jdevelops.redis;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import io.lettuce.core.RedisConnectionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionCommands;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.util.Arrays;
 
