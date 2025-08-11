adb5aa21b3dfd146c79596a0596eb6a34284d2c3
tan <13320385140@163.com>
2025-08-11 16:16:24 +0800

    fix: [renewpwd] 2

---
+++ jdevelops-dals/jdevelops-dals-renewpwd/src/main/java/cn/tannn/jdevelops/renewpwd/DefaultConfigsRefreshHandler.java
@@ -0,0 +1,155 @@
+package cn.tannn.jdevelops.renewpwd;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBindingPostProcessor;
+
+import javax.sql.DataSource;
+import java.util.*;
+
+/**
+ * 默认配置刷新处理器实现
+ *
+ * @author <a href="https://tannn.cn/">tnnn</a>
+ * @version V1.0
+ * @date 2024/5/2 下午8:49
+ */
+@Component
+public class DefaultConfigsRefreshHandler implements ConfigsRefreshHandler, ApplicationContextAware {
+
+
+    private static final Logger log = LoggerFactory.getLogger(DefaultConfigsRefreshHandler.class);
+
+    private ConfigurableApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = (ConfigurableApplicationContext) applicationContext;
+    }
+
+
+
+    @Override
+    public void refreshDataSourceConfig() {
+        log.info("[renewpwd] 开始刷新数据源配置");
+        try {
+            // 1. 重新绑定数据源配置属性
+            rebindDataSourceProperties();
+            // 2. 重建数据源 Bean
+            refreshDataSourceBeans();
+            log.info("[renewpwd] 数据源配置刷新完成");
+        } catch (Exception e) {
+            log.error("[renewpwd] 数据源配置刷新失败", e);
+        }
+    }
+
+    /**
+     * 重新绑定数据源配置属性
+     */
+    private void rebindDataSourceProperties() {
+        try {
+            ConfigurationPropertiesBindingPostProcessor bindingPostProcessor =
+                    applicationContext.getBean(ConfigurationPropertiesBindingPostProcessor.class);
+
+            Map<String, Object> dataSourceBeans = applicationContext.getBeansWithAnnotation(
+                    org.springframework.boot.context.properties.ConfigurationProperties.class);
+
+            for (Map.Entry<String, Object> entry : dataSourceBeans.entrySet()) {
+                String beanName = entry.getKey();
+                Object bean = entry.getValue();
+
+                // 检查是否是数据源配置 Bean
+                org.springframework.boot.context.properties.ConfigurationProperties annotation =
+                        bean.getClass().getAnnotation(org.springframework.boot.context.properties.ConfigurationProperties.class);
+
+                if (annotation != null && annotation.prefix().startsWith("spring.datasource")) {
+                    bindingPostProcessor.postProcessBeforeInitialization(bean, beanName);
+                    log.info("[renewpwd] 重新绑定数据源配置 Bean: {}", beanName);
+                }
+            }
+        } catch (Exception e) {
+            log.warn("[renewpwd] 重新绑定数据源配置失败", e);
+        }
+    }
+
+    /**
+     * 重建数据源相关 Bean
+     */
+    private void refreshDataSourceBeans() {
+        DefaultListableBeanFactory beanFactory =
+                (DefaultListableBeanFactory) applicationContext.getBeanFactory();
+
+        // 获取所有数据源 Bean
+        String[] dataSourceBeans = applicationContext.getBeanNamesForType(DataSource.class);
+
+        for (String beanName : dataSourceBeans) {
+            try {
+                if (beanFactory.containsSingleton(beanName)) {
+                    // 关闭旧的数据源连接
+                    closeDataSourceGracefully(beanName);
+
+                    // 销毁 Bean，让 Spring 重新创建
+                    beanFactory.destroySingleton(beanName);
+
+                    log.info("[renewpwd] 标记数据源 Bean 需要重新创建: {}", beanName);
+                }
+            } catch (Exception e) {
+                log.warn("[renewpwd] 刷新数据源 Bean 失败: {}", beanName, e);
+            }
+        }
+
+        // 同时刷新 JdbcTemplate（如果存在）
+        String[] jdbcTemplateBeans = applicationContext.getBeanNamesForType(
+                org.springframework.jdbc.core.JdbcTemplate.class);
+
+        for (String beanName : jdbcTemplateBeans) {
+            try {
+                if (beanFactory.containsSingleton(beanName)) {
+                    beanFactory.destroySingleton(beanName);
+                    log.info("[renewpwd] 标记 JdbcTemplate Bean 需要重新创建: {}", beanName);
+                }
+            } catch (Exception e) {
+                log.warn("[renewpwd] 刷新 JdbcTemplate Bean 失败: {}", beanName, e);
+            }
+        }
+    }
+
+    /**
+     * 优雅关闭数据源
+     */
+    private void closeDataSourceGracefully(String beanName) {
+        try {
+            Object bean = applicationContext.getBean(beanName);
+
+            // 如果 Bean 实现了 DisposableBean 接口，调用 destroy 方法
+            if (bean instanceof org.springframework.beans.factory.DisposableBean) {
+                ((org.springframework.beans.factory.DisposableBean) bean).destroy();
+                log.debug("[renewpwd] 调用 DisposableBean.destroy() 方法: {}", beanName);
+            }
+
+            // 如果 Bean 实现了 AutoCloseable 接口，调用 close 方法
+            if (bean instanceof AutoCloseable) {
+                ((AutoCloseable) bean).close();
+                log.debug("[renewpwd] 调用 AutoCloseable.close() 方法: {}", beanName);
+            }
+
+            // 如果 Bean 实现了 Closeable 接口，调用 close 方法
+            if (bean instanceof java.io.Closeable) {
+                ((java.io.Closeable) bean).close();
+                log.debug("[renewpwd] 调用 Closeable.close() 方法: {}", beanName);
+            }
+
+        } catch (Exception e) {
+            log.debug("[renewpwd] Bean 销毁过程中出现异常: {}", beanName, e);
+        }
+    }
+
+}
