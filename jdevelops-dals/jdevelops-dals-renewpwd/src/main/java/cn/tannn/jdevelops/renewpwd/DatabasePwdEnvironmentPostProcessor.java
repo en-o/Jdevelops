65fdc1559d0fafe15af231d27eba8d4246943fd6
tan <13320385140@163.com>
2025-08-13 15:33:15 +0800

    fix: [renewpwd] 5

--- jdevelops-dals/jdevelops-dals-renewpwd/src/main/java/cn/tannn/jdevelops/renewpwd/DatabasePwdEnvironmentPostProcessor.java
+++ jdevelops-dals/jdevelops-dals-renewpwd/src/main/java/cn/tannn/jdevelops/renewpwd/DatabasePwdEnvironmentPostProcessor.java
@@ -1,23 +1,24 @@
 package cn.tannn.jdevelops.renewpwd;
 
-import org.springframework.beans.BeansException;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.env.EnvironmentPostProcessor;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.*;
 import java.util.HashMap;
 
 /**
- * 数据库密码处理器
+ * 数据库密码处理器 - 启动时对密码进行处理
  * <P> 1. 使用密码池密码 </P>
  * <P> 2. 密码解密 </P>
  */
-public class DatabasePwdEnvironmentPostProcessor implements EnvironmentPostProcessor {
+public class DatabasePwdEnvironmentPostProcessor implements BeanFactoryPostProcessor, EnvironmentAware {
+
+    private Environment environment;
 
     @Override
-    public void postProcessEnvironment(ConfigurableEnvironment env, SpringApplication app) {
-        String password = env.getProperty("spring.datasource.password");
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) {
+        ConfigurableEnvironment ENV = (ConfigurableEnvironment) environment;
+        String password = ENV.getProperty("spring.datasource.password");
         // 需要解密
         if (password != null && password.startsWith("ENC(")) {
             String encrypted = password.substring(4, password.length() - 1);
@@ -25,9 +26,15 @@ public class DatabasePwdEnvironmentPostProcessor implements EnvironmentPostProce
         }
         HashMap<String, Object> map = new HashMap<>();
         map.put("spring.datasource.password", password);
-        env.getPropertySources().addFirst(
-                new MapPropertySource("decrypted", map)
+        ENV.getPropertySources().addFirst(
+                new MapPropertySource("renewpwdConfigPropertySource", map)
         );
+    }
+
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 
 
