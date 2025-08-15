8d09cc4f360f423a472dd20ff3f2781ca5494c63
tan <13320385140@163.com>
2025-08-15 16:34:57 +0800

    fix: [renewpwd] 覆盖spring默认DataSourceAutoConfiguration

--- jdevelops-dals/jdevelops-dals-renewpwd/src/main/java/cn/tannn/jdevelops/renewpwd/refresh/RenewpwdDataSourceConfig.java
+++ jdevelops-dals/jdevelops-dals-renewpwd/src/main/java/cn/tannn/jdevelops/renewpwd/refresh/RenewpwdDataSourceConfig.java
@@ -4,6 +4,7 @@ import cn.tannn.jdevelops.renewpwd.refresh.dataconfig.DataSourceConfigStrategy;
 import cn.tannn.jdevelops.renewpwd.refresh.dataconfig.HikariConfigStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,7 @@ import java.util.List;
  * 通过 @RefreshScope 实现动态刷新
  */
 @Configuration(proxyBeanMethods = false)
+@AutoConfigureBefore(org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class)
 public class RenewpwdDataSourceConfig {
 
     private static final Logger log = LoggerFactory.getLogger(RenewpwdDataSourceConfig.class);
