9a4a98aaa8d030278ed587c061fd54a631ed3da3
tan <13320385140@163.com>
2025-08-15 13:23:17 +0800

    fix: [renewpwd] 123

--- jdevelops-dals/jdevelops-dals-renewpwd/src/main/java/cn/tannn/jdevelops/renewpwd/PwdCheckDetector.java
+++ jdevelops-dals/jdevelops-dals-renewpwd/src/main/java/cn/tannn/jdevelops/renewpwd/PwdCheckDetector.java
@@ -3,9 +3,7 @@ package cn.tannn.jdevelops.renewpwd;
 import cn.tannn.jdevelops.renewpwd.pojo.PwdExpireInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 
 import java.time.LocalDateTime;
 import java.util.concurrent.ScheduledExecutorService;
