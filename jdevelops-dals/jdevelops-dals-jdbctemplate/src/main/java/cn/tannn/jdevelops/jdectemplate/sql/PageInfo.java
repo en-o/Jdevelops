700072c5a87fd349507b747163cca4d85fc71c42
tan <13320385140@163.com>
2025-09-24 13:06:00 +0800

    feat: [jdectemplate] PageInfo

--- jdevelops-dals/jdevelops-dals-jdbctemplate/src/main/java/cn/tannn/jdevelops/jdectemplate/sql/PageInfo.java
+++ jdevelops-dals/jdevelops-dals-jdbctemplate/src/main/java/cn/tannn/jdevelops/jdectemplate/sql/PageInfo.java
@@ -18,4 +18,28 @@ public class PageInfo {
      * 数量
      */
     private Integer pageSize;
+
+    public Integer getPageIndex() {
+        return pageIndex;
+    }
+
+    public void setPageIndex(Integer pageIndex) {
+        this.pageIndex = pageIndex;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    @Override
+    public String toString() {
+        return "PageInfo{" +
+                "pageIndex=" + pageIndex +
+                ", pageSize=" + pageSize +
+                '}';
+    }
 }
