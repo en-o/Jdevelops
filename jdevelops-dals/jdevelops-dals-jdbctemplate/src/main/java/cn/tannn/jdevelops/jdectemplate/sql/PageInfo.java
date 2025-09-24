86332053013ec788969973c364f4800755d64193
tan <13320385140@163.com>
2025-09-24 12:52:14 +0800

    feat: [jdectemplate] PageInfo

---
+++ jdevelops-dals/jdevelops-dals-jdbctemplate/src/main/java/cn/tannn/jdevelops/jdectemplate/sql/PageInfo.java
@@ -0,0 +1,21 @@
+package cn.tannn.jdevelops.jdectemplate.sql;
+
+/**
+ * 分页参数
+ *
+ * @author <a href="https://t.tannn.cn/">tan</a>
+ * @version V1.0
+ * @date 2025/9/24 12:50
+ */
+public class PageInfo {
+
+    /**
+     * 页码
+     */
+    private Integer pageIndex;
+
+    /**
+     * 数量
+     */
+    private Integer pageSize;
+}
