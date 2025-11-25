0d8f5ab4c569f24d9888d32ba489fa6441bf7915
tan <13320385140@163.com>
2025-11-25 17:01:56 +0800

    feat: [p6spy] 追加P6spyConfig参数

--- jdevelops-logs/jdevelops-logs-p6spy/src/test/java/cn/tannn/jdevelops/p6spy/P6spyUtilsTest.java
+++ jdevelops-logs/jdevelops-logs-p6spy/src/test/java/cn/tannn/jdevelops/p6spy/P6spyUtilsTest.java
@@ -9,5 +9,6 @@ class P6spyUtilsTest {
     void checkDriverClasses() {
         // 测试的是时候注释点  class.from
 //        System.out.println(P6spyUtils.checkDriverClasses(new P6spyConfig().getDrivers()));
+//        System.out.println(Boolean.FALSE+"");
     }
 }
